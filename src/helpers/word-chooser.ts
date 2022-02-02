70925c6d976878c61a9143ddd85676afc558fbcc
Isaac Lyman <isaaclyman@gmail.com>
2022-02-01 19:38:25 -0700

    Create word-chooser

---
+++ src/helpers/word-chooser.ts
@@ -0,0 +1,14 @@
+import commonTxt from '../../resources/common_words_6.txt'
+
+const arrayOfCommonWords = (commonTxt as string).split(/\r?\n/)
+const numberOfCommonWords = arrayOfCommonWords.length
+
+const now = new Date()
+const anchor = new Date(2020, 0, 1)
+
+const msInDay = 1000 * 60 * 60 * 24
+
+const daysSinceAnchor = Math.floor((now.getTime() - anchor.getTime()) / msInDay)
+const constrainedDay = daysSinceAnchor % numberOfCommonWords
+
+export const todaysWord = arrayOfCommonWords[constrainedDay]
